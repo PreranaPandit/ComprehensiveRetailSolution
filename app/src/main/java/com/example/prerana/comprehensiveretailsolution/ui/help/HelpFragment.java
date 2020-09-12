56ec5337721c7f6f5f8db5c078c7410c08487dd4
PreranaPandit <preranapandit07@gmail.com>
2020-09-12 18:12:43 +0545

    Help and Logout navigation completion

--- app/src/main/java/com/example/prerana/comprehensiveretailsolution/ui/help/HelpFragment.java
+++ app/src/main/java/com/example/prerana/comprehensiveretailsolution/ui/help/HelpFragment.java
@@ -1,38 +1,50 @@
 package com.example.prerana.comprehensiveretailsolution.ui.help;
 
-import androidx.lifecycle.ViewModelProviders;
-
+import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProviders;
 
 import com.example.prerana.comprehensiveretailsolution.R;
 
 public class HelpFragment extends Fragment {
 
-    private HelpViewModel mViewModel;
+    private HelpViewModel helpViewModel;
 
-    public static HelpFragment newInstance() {
-        return new HelpFragment();
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        helpViewModel =
+                ViewModelProviders.of(this).get(HelpViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_help, container, false);
+//        final TextView textView = root.findViewById(R.id.text_share);
+//        shareViewModel.getText().observe(this, new Observer<String>() {
+//            @Override
+//            public void onChanged(@Nullable String s) {
+//                tex
+
+
+//        Intent emailIntent = new Intent(Intent.ACTION_SEND);
+//        emailIntent.setType("text/plain");
+//        startActivity(emailIntent);
+
+        Intent intent=new Intent(Intent.ACTION_SEND);
+        String[] recipients={"2020comprehensive@retails.com"};
+        intent.putExtra(Intent.EXTRA_EMAIL, recipients);
+        intent.putExtra(Intent.EXTRA_SUBJECT,"Subject text here...");
+        intent.putExtra(Intent.EXTRA_TEXT,"Please write your issues or suggestions here...");
+        intent.putExtra(Intent.EXTRA_CC,"mailcc@gmail.com");
+        intent.setType("text/html");
+        intent.setPackage("com.google.android.gm");
+        startActivity(Intent.createChooser(intent, "Send mail"));
+
+
+        return root;
+
     }
-
-    @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
-                             @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_help, container, false);
-    }
-
-    @Override
-    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-        mViewModel = ViewModelProviders.of(this).get(HelpViewModel.class);
-        // TODO: Use the ViewModel
-    }
-
 }
--- app/src/main/java/com/example/prerana/comprehensiveretailsolution/ui/logout/LogoutFragment.java
+++ app/src/main/java/com/example/prerana/comprehensiveretailsolution/ui/logout/LogoutFragment.java
@@ -1,36 +1,139 @@
 package com.example.prerana.comprehensiveretailsolution.ui.logout;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.example.prerana.comprehensiveretailsolution.R;
+import com.example.prerana.comprehensiveretailsolution.activity.LoginActivity;
+import com.example.prerana.comprehensiveretailsolution.api.UsersAPI;
+import com.example.prerana.comprehensiveretailsolution.url.Url;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class LogoutFragment extends Fragment {
 
-    private LogoutViewModel mViewModel;
+    private LogoutViewModel logOutViewModel;
 
-    public static LogoutFragment newInstance() {
-        return new LogoutFragment();
+    private Button btnLogOut;
+    AlertDialog.Builder builder;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        logOutViewModel =
+                ViewModelProviders.of(this).get(LogoutViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_logout, container, false);
+        final TextView textView = root.findViewById(R.id.text_send);
+//        sendViewModel.getText().observe(this, new Observer<String>() {
+//            @Override
+//            public void onChanged(@Nullable String s) {
+//                textView.setText(s);
+//            }
+//        });
+
+
+
+        btnLogOut = root.findViewById(R.id.btnLogOut);
+        builder = new AlertDialog.Builder(getContext());
+
+
+        btnLogOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+
+                builder.setMessage("Are you sure to get Logout? ")
+                        .setCancelable(false)
+                        .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                logout();
+                                //  Toast.makeText(getContext(), "You are logout successfully !", Toast.LENGTH_SHORT).show();
+                            }
+                        })
+                        .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                //Action for no button clicked
+                                dialog.cancel();
+                                // Toast.makeText(getContext(), "You  !", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+
+                //creating dialog box
+                AlertDialog alert = builder.create();
+                alert.setTitle(" Alert! ");
+                alert.show();
+
+
+
+
+
+
+            }
+        });
+
+
+        return root;
     }
 
+
+
+    public void logout(){
+        UsersAPI usersAPI = Url.getInstance().create(UsersAPI.class);
+        Call<Void> userCall = usersAPI.logout(Url.token);
+
+        userCall.enqueue(new Callback<Void>() {
+            @Override
+            public void onResponse(Call<Void> call, Response<Void> response) {
+//                        if(!response.isSuccessful()){
+//                            Toast.makeText(getActivity(), "Code" + response.code(), Toast.LENGTH_SHORT).show();
+//                            return;
+//                        }
+
+
+                Intent intent = new Intent(getContext(), LoginActivity.class);
+                startActivity(intent);
+                Toast.makeText(getContext(), "Logout Successful", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onFailure(Call<Void> call, Throwable t) {
+
+            }
+        });
+    }
+
+
+    //to hide title from account fragment
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
-                             @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_logout, container, false);
+    public void onResume()
+    {
+        super.onResume();
+        ((AppCompatActivity)getActivity()).getSupportActionBar().hide();
+    }
+    @Override
+    public void onStop()
+    {
+        super.onStop();
+        ((AppCompatActivity)getActivity()).getSupportActionBar().show();
+
     }
 
-    @Override
-    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-        mViewModel = ViewModelProviders.of(this).get(LogoutViewModel.class);
-        // TODO: Use the ViewModel
-    }
 
 }
